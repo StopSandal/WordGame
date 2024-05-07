345926744065ce4a5e1feaf0a1648d916bdc2e80
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-05-07 17:15:03 +0300

    unstage test

---
+++ Data/GameResultItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordGame.Data
+{
+    internal class GameResultItem
+    {
+        public int Score { get; set; }
+        public string PlayerName { get; set; }
+
+        public GameResultItem()
+        {
+            Score = 0;
+            PlayerName = String.Empty;
+        }
+    }
+}
